98204563b1f6bc3ea9a449d5d0c71e55f6865506
johnstar <756053621@qq.com>
2016-05-01 21:43:27 +0800

    use to test

---
+++ src/com/atguigu/git/Git.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.atguigu.git;
+
+/**
+ * @author johnstar
+ *
+ * 2016年5月1日
+ */
+public class Git {
+
+	public static void main(String[] args) {
+		System.out.println("git success");
+	}
+}
